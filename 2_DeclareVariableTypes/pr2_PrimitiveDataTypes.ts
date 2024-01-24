a7e711d160ac22650e581ea71c11adc0ec5a7938
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-01-24 22:38:41 +0530

    Updated .ts file

---
+++ 2_DeclareVariableTypes/pr2_PrimitiveDataTypes.ts
@@ -0,0 +1,27 @@
+// Example 1
+console.log("__Boolean Type__");
+let flag: Boolean;
+let yes = true;
+let no = false;
+
+
+// Example 2
+console.log("__Number and BigInteger types__");
+let x: Number;
+let y = 0;
+let z: Number = 123.456;
+let big: BigInt = 100n;
+
+
+
+// Example 3
+console.log("__String Type__");
+let s: String;
+let empty = "";
+let abc = 'abc';
+
+
+// Example 4
+let firstName: String = "सौम्यदीप मजूमदार";
+let sentence: String = `टाइपस्क्रिप्ट लर्निंग में आपका स्वागत है ${firstName} आप VSCode का उपयोग कर रहे हैं।`;
+console.log(sentence);
