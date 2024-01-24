d148c42b612b5df2b912d847f136e23ebbb1bb67
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-01-24 15:30:14 +0530

    Updated folder

---
+++ 1_GetStartedWithTypeScript/pr1.ts
@@ -0,0 +1,17 @@
+// Example 1
+console.log("__Example 1__");
+function addTwoNumbers(x, y) {
+    return x+y;
+}
+
+console.log(addTwoNumbers(4, 6));
+
+
+// Example 2
+console.log("__Example 2__");
+function आपका_स्वागत_हैअभिवादन(तुम्हारा_नाम) {
+    console.log(`${तुम्हारा_नाम} आपका स्वागत है, \nयह टाइपस्क्रिप्ट में सीखने की यात्रा है`);
+    return 0;
+}
+
+आपका_स्वागत_हैअभिवादन('Soumyadip');
