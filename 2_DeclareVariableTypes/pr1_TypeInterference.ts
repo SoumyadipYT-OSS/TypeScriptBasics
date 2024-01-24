606bcabbbec078154c3937f7552de201e17b4540
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-01-24 16:15:29 +0530

    Updated

---
+++ 2_DeclareVariableTypes/pr1_TypeInterference.ts
@@ -0,0 +1,4 @@
+let x: Number;  // Explicity declares x as a number type
+let y = 1;   // Implicity declares y as a number type
+let z;   // Declares z without initializing it or type
+let s: String;  // Explicity declares s as a string type
