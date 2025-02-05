1ef512209e0c3cd6d700de2d6d5a1dd697fc232e
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2025-02-05 12:00:01 +0000

    TypeScript initialized repository

--- 2_DeclareVariableTypes/pr1_TypeInterference.ts
+++
@@ -1,4 +0,0 @@
-let x: Number;  // Explicity declares x as a number type
-let y = 1;   // Implicity declares y as a number type
-let z;   // Declares z without initializing it or type
-let s: String;  // Explicity declares s as a string type
