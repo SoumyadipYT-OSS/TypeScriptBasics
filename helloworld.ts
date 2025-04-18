e33a1abdce308764f740ceabf386fd1970fa7c55
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-04-18 12:16:29 +0530

    First program of typescript is added

---
+++ helloworld.ts
@@ -0,0 +1,8 @@
+let message: string = "Welcome to TypeScript!";
+console.log(message);
+
+let word: string = "TypeScript";
+console.log(word);
+
+let deviceName: string = "hp";
+console.log(deviceName);
